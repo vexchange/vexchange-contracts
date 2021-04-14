278cb2ec9bc9b32078e84cfb59e0406e8ab1c6e9
Oliver Chalk <oliverchalk245@gmail.com>
2021-04-14 15:57:27 +1000

    V2Factory: Add the new methods and properties to interface

--- uniswap-v2-periphery/contracts/ext-v2-core/IUniswapV2Factory.sol
+++ uniswap-v2-periphery/contracts/ext-v2-core/IUniswapV2Factory.sol
@@ -4,14 +4,26 @@ interface IUniswapV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
     function feeTo() external view returns (address);
-    function feeToSetter() external view returns (address);
 
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
-    function allPairs(uint) external view returns (address pair);
+    function defaultSwapFee() external view returns (uint);
+    function defaultPlatformFee() external view returns (uint);
+    function defaultRecoverer() external view returns (address);
+
     function allPairsLength() external view returns (uint);
 
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
     function setFeeTo(address) external;
-    function setFeeToSetter(address) external;
+
+    function setDefaultSwapFee(uint) external;
+    function setDefaultPlatformFee(uint) external;
+    function setDefaultRecoverer(address) external;
+
+    function setSwapFeeForPair(address, uint) external;
+    function setPlatformFeeForPair(address, uint) external;
+    function setRecovererForPair(address, address) external;
+
+    function MAX_PLATFORM_FEE() external view returns (uint);
+    function MIN_SWAP_FEE() external view returns (uint);
+    function MAX_SWAP_FEE() external view returns (uint);
 }
