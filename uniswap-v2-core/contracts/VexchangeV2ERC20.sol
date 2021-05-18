636186ec2b6e7fc0b2f907eb41184a439ca80408
Ian McLeod <ian@leodsoft.com>
2021-05-18 11:36:01 +1000

    Contract rename UniswapV2 ==> VexchangeV2

--- uniswap-v2-core/contracts/VexchangeV2ERC20.sol
+++ uniswap-v2-core/contracts/VexchangeV2ERC20.sol
@@ -1,9 +1,9 @@
 pragma solidity =0.5.16;
 
-import './interfaces/IUniswapV2ERC20.sol';
+import './interfaces/IVexchangeV2ERC20.sol';
 import './libraries/SafeMath.sol';
 
-contract UniswapV2ERC20 is IUniswapV2ERC20 {
+contract VexchangeV2ERC20 is IVexchangeV2ERC20 {
     using SafeMath for uint;
 
     string public constant name = 'Vexchange V2';
